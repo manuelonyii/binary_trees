7a5000ccce1a55ce96ff240f3fababcf857732e0
manuelonyii <edvardjonas7@gnas7@gmail.com>
2023-03-03 15:55:52 -0800

    text

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: a pointer to the root node of the tree to delete
- * Description: If tree is NULL, do nothing
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node to check
+ *
+ * Return: 1 if node is a leaf
+ *         0 if not a leaf
+ *         0 if node is NULL
  */
-
-void binary_tree_delete(binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!tree)
-		return;
-	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->left);
-	free(tree);
-}
+	return ((!node || node->left || node->right) ? 0 : 1);
+}}
